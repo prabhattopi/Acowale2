1d7d1be1d4f4a4af965bb23e8d6e9b2eaca594da
prabhattopi <topi9864@gmail.com>
2024-10-05 13:02:25 +0530

    the file is save with full stack app

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+import type { Config } from 'tailwindcss';
+
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  './node_modules/preline/preline.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [
+    require('preline/plugin'),
+],
+};
+
+export default config;
